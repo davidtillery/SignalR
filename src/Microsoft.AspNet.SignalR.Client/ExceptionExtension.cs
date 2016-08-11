ba1a4f2e2d19761cd59850f9b2c4ba346ae25c33
David Tillery <david.tillery@hp.com>
2016-08-11 16:08:56 -0400

    Converted Client from csproj to xproj.  VS was happier this way.  I could see the files in the solution, and it fixed the Resources.resx not being included problem.

---
+++ src/Microsoft.AspNet.SignalR.Client/ExceptionExtension.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Microsoft.AspNet.SignalR
+{
+    public static class ExceptionExtension
+    {
+        public static Exception Unwrap(this Exception ex)
+        {
+            return ex;
+        }
+    }
+}
